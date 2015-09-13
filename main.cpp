87313c262655d28f9801b6a635d51d46b6a88083
Yuriy Astrov <yuriastrov@gmail.com>
2015-09-13 20:24:10 +0300

    Main cpp has been fixed for distribution.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QFileInfo>
+#include <QCoreApplication>
 
 int main(int argc, char *argv[])
 {
+    QStringList paths = QCoreApplication::libraryPaths();
+    paths.append(".");
+    paths.append("platforms");
+    QCoreApplication::setLibraryPaths(paths);
+
     QApplication a(argc, argv);
     a.setApplicationVersion(APP_VERSION);
+
     MainWindow w;
     w.show();
 
